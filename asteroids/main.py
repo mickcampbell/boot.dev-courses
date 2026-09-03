ee6dfc002a3b5218a66666853227baeafecd505d
mickcampbell <michael@campbells.id.au>
2026-09-03 20:01:07 +1000

    Player go vroom

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     #print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -13,15 +14,29 @@ def main():
     pygame.time.Clock()
     dt = 0.0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player.draw(screen)
+
+
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
 
         dt = pygame.time.Clock().tick(60) / 1000
+
 
 
 if __name__ == "__main__":
